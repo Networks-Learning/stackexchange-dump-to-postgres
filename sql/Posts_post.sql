f42cc9dd1ef0daaa536538d2eb9a560f9fda6aef
Utkarsh Upadhyay <musically.ut@gmail.com>
2015-01-08 17:30:32 +0100

    Add composite indexes to avoid touching the table while filtering for Q/A.

--- sql/Posts_post.sql
+++ sql/Posts_post.sql
@@ -13,3 +13,9 @@ CREATE INDEX posts_comment_count_idx on Posts USING btree (CommentCount)
        WITH (FILLFACTOR = 100);
 CREATE INDEX posts_favorite_count_idx on Posts USING btree (FavoriteCount)
        WITH (FILLFACTOR = 100);
+
+-- Composite indexes (optional)
+CREATE INDEX posts_id_post_type_id_idx on Posts USING btree (Id, PostTypeId)
+       WITH (FILLFACTOR = 100);
+CREATE INDEX posts_id_parent_id_idx on Posts USING btree (Id, ParentId)
+       WITH (FILLFACTOR = 100);
