ab332064f7a2708ec9364f1389b3aa929d0762d4
Utkarsh Upadhyay <musically.ut@gmail.com>
2015-01-20 15:50:12 +0100

    Add another composite index on Posts.
    The index on OwnerUserId and CreationDate helps in queries which group
    or partition based on these attributes.

--- sql/Posts_post.sql
+++ sql/Posts_post.sql
@@ -1,25 +1,27 @@
 -- hash index takes too long to create
-CREATE INDEX posts_post_type_id_idx on Posts USING btree (PostTypeId)
+CREATE INDEX posts_post_type_id_idx ON Posts USING btree (PostTypeId)
        WITH (FILLFACTOR = 100);
-CREATE INDEX posts_score_idx on Posts USING btree (Score)
+CREATE INDEX posts_score_idx ON Posts USING btree (Score)
        WITH (FILLFACTOR = 100);
-CREATE INDEX posts_creation_date_idx on Posts USING btree (CreationDate)
+CREATE INDEX posts_creation_date_idx ON Posts USING btree (CreationDate)
        WITH (FILLFACTOR = 100);
-CREATE INDEX posts_owner_user_id_idx on Posts USING hash (OwnerUserId)
+CREATE INDEX posts_owner_user_id_idx ON Posts USING hash (OwnerUserId)
        WITH (FILLFACTOR = 100);
-CREATE INDEX posts_answer_count_idx on Posts USING btree (AnswerCount)
+CREATE INDEX posts_answer_count_idx ON Posts USING btree (AnswerCount)
        WITH (FILLFACTOR = 100);
-CREATE INDEX posts_comment_count_idx on Posts USING btree (CommentCount)
+CREATE INDEX posts_comment_count_idx ON Posts USING btree (CommentCount)
        WITH (FILLFACTOR = 100);
-CREATE INDEX posts_favorite_count_idx on Posts USING btree (FavoriteCount)
+CREATE INDEX posts_favorite_count_idx ON Posts USING btree (FavoriteCount)
        WITH (FILLFACTOR = 100);
-CREATE INDEX posts_viewcount_idx on Posts USING btree (ViewCount)
+CREATE INDEX posts_viewcount_idx ON Posts USING btree (ViewCount)
        WITH (FILLFACTOR = 100);
 
 -- Composite indexes (optional)
-CREATE INDEX posts_id_post_type_id_idx on Posts USING btree (Id, PostTypeId)
+CREATE INDEX posts_id_post_type_id_idx ON Posts USING btree (Id, PostTypeId)
        WITH (FILLFACTOR = 100);
-CREATE INDEX posts_id_parent_id_idx on Posts USING btree (Id, ParentId)
+CREATE INDEX posts_id_parent_id_idx ON Posts USING btree (Id, ParentId)
        WITH (FILLFACTOR = 100);
-CREATE INDEX posts_id_accepted_answers_id_idx on Posts USING btree (Id, AcceptedAnswerId)
+CREATE INDEX posts_id_accepted_answers_id_idx ON Posts USING btree (Id, AcceptedAnswerId)
+       WITH (FILLFACTOR = 100);
+CREATE INDEX posts_owner_user_id_creation_date_idx ON Posts USING btree (OwnerUserId, CreationDate)
        WITH (FILLFACTOR = 100);
