f3098a11c183ef178a58cf25a75291857ff87346
airborne <fibinse.xavier@gmail.com>
2016-12-08 12:52:14 +0530

    ENH: Adds queries to create accepted_answer_id and qn_ans_timing tables

--- sql/PostHistory_post.sql
+++ sql/PostHistory_post.sql
@@ -9,3 +9,26 @@ CREATE INDEX ph_creation_date_idx ON PostHistory USING btree (CreationDate)
        WITH (FILLFACTOR = 100);
 CREATE INDEX ph_userid_idx ON PostHistory USING btree (UserId)
        WITH (FILLFACTOR = 100);
+
+
+CREATE TABLE accepted_answer_id as SELECT DISTINCT acceptedanswerid FROM posts;
+
+DROP TABLE qn_ans_timing;
+
+CREATE TABLE qn_ans_timing AS
+SELECT 
+	p.id, 
+	p.tags, 
+	p.owneruserid, 
+	p.creationdate as qn_creation_ts, 
+	h.creationdate as ans_creation_ts, 
+	p.acceptedanswerid,
+	h.userid as answered_by
+FROM posts p, posthistory h
+WHERE p.acceptedanswerid is not null
+AND h.postid in (
+				SELECT * 
+				FROM accepted_answer_id
+				)
+
+AND p.acceptedanswerid=h.postid;
