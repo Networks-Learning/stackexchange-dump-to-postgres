47759e7c1e7706cdf863b5d5c1888e72409fdb22
madtibo <madtibo_git@tribu-ml.fr>
2018-08-16 17:43:49 +0200

    revert to post columns as not null

--- sql/PostLinks_pre.sql
+++ sql/PostLinks_pre.sql
@@ -2,8 +2,8 @@ DROP TABLE IF EXISTS PostLinks CASCADE;
 CREATE TABLE PostLinks (
    Id                int         PRIMARY KEY ,
    CreationDate      timestamp   not NUll    ,
-   PostId            int         , -- not NULL    ,
-   RelatedPostId     int         , -- not NULL    ,
+   PostId            int         not NULL    ,
+   RelatedPostId     int         not NULL    ,
    LinkTypeId        int         not Null    ,
    jsonfield         jsonb
 );
